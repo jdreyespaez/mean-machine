1297ec246dc117a5fea088e1c84637104662c0b8
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2016-01-24 14:11:27 +0800

    1 commit

---
+++ 86-nodeApi/server.js
@@ -0,0 +1,54 @@
+// LLAMANDO LOS PAQUETES
+var express     = require('express');
+var app         = express(); // definir la app con express
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan'); // usado para ver los requests
+var mongoose    = require('mongoose');
+var User        = require('./app/models/user');
+var port        = process.env.PORT || 8080;
+
+// conectando con la db en modulus.io
+mongoose.connect('mongodb://jdreyespaez:modulus@meanmachine-55307.onmodulus.net')
+
+// CONFIGURACIÓN DE LA APP
+// usando body parser para tomar los datos POST en JSON
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+// configurar el app para que haga CORS requests
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+
+  next();
+});
+
+// cargue todos los requests en la consola
+app.use(morgan('dev'));
+
+// RUTAS DE LA API
+
+// ruta básica del inicio
+app.get('/', function(req, res) {
+  res.send('¡Bienvenido a la página de inicio!');
+});
+
+// tomar una instancia del enrutador de express
+var apiRouter = express.Router();
+
+// testeando que se puede acceder al API
+// PostMan: GET hht://localhost:8080/api
+apiRouter.get('/', function(req, res) {
+  res.json({ message: 'Yey! Bienvenido al api!' });
+});
+
+// Aquí habrán más rutas
+
+// MARCANDO LAS RUTAS
+// todas las rutas tendrán como prefijo /api
+app.use('/api', apiRouter);
+
+// INICIANDO EL SERVIDOR
+app.listen(port);
+console.log('La magia pasa en el puerto' + port);
