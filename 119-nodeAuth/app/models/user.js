2da30c1045c84b99b881d37e355332b3d6494be4
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2016-01-24 16:58:59 +0800

    NEW CHAPTER: 119-Node Authentication

---
+++ 119-nodeAuth/app/models/user.js
@@ -0,0 +1,38 @@
+// tomar los paqetes que necesitamos para el modelo user
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+// user Schema
+var UserSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, index: { unique: true }},
+  password: { type: String, required: true, select: false }
+});
+
+// esconder el password antes de que el usuario lo guarde
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  // esconder el password antes que sea cambiado o sea un usuario nuevo
+  if (!user.isModified('password')) return next();
+
+  // crear el hash
+  bcrypt.hash(user.password, null, null, function(err, hash) {
+    if (err) return next(err);
+
+    // cambiar a la versión hasheada
+    user.password = hash;
+    next();
+  });
+});
+
+// método para comparar un password con el hash de la DB
+UserSchema.methods.comparePassword = function(password) {
+  var user = this;
+
+  return bcrypt.compareSync(password, user.password);
+};
+
+// devolver el modelo
+module.exports = mongoose.model('User', UserSchema);
