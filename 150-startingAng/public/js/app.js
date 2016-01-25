e4a8b5a98950c191e3274d5cd2efff527cb73d57
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2016-01-25 11:13:02 +0800

    Módulos y controladores + Aplicando la Angular Application

---
+++ 150-startingAng/public/js/app.js
@@ -0,0 +1,34 @@
+angular.module('routerApp', [])
+
+// creando los controladores
+// éste será el que maneje TODO el siio
+.controller('mainController', function() {
+
+  var vm = this;
+
+  // crear un mensaje grande para mostrar en la vista
+  vm.bigMessage = 'Un mar tranquilo nunca hará a un marino hábil.';
+})
+// el controlador específico del inicio
+.controller('homeController', function() {
+
+  var vm = this;
+
+  vm.message = '¡Ésta es la página de inicio!';
+})
+
+// el controlador de la página de nosotros
+.controller('nosotrosController', function() {
+
+  var vm = this;
+
+  vm.message = '¡Ésta es la página sobre nosotros!';
+})
+
+// el controlador de la página de contacto
+.controller('contactoController', function() {
+
+  var vm = this;
+
+  vm.message = '¡Ésta es la página de contacto!';
+});
