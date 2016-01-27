9f1ed56319e9f9f622b011941e15873da42443d7
Juan-David Reyes-Páez <jd.reyespaez@gmail.com>
2016-01-27 13:14:42 +0800

    completing delete user functionality

--- MEAN-crm/public/app/controllers/userCtrl.js
+++ MEAN-crm/public/app/controllers/userCtrl.js
@@ -35,4 +35,31 @@ angular.module('userCtrl', ['userService'])
       });
   };
 
-});
+})
+
+// controller applied to user creation page
+.controller('userCreateController', function(User) {
+  var vm = this;
+
+  // this variable hide/show elements of the views
+  // differentiates between create or edit pages
+  vm.type = 'create';
+
+  // function to create a user
+  vm.saveUser = function() {
+    vm.processing = true;
+    // clear the message
+    vm.message = '';
+
+    // use the create function in the userService
+    User.create(vm.userData)
+      .success(function(data) {
+        vm.processing = false;
+
+        // clear the form
+        vm.userData = {};
+        vm.message = data.message;
+      });
+
+  };
+})
